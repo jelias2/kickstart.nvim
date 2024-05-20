a24982c92c329bbb72ad211678bb0b8d06f54682
Jacob Elias <jacob@jacobs-apple-macbook-pro.phoenix-vibes.ts.net>
2024-05-20 12:02:53 -0500

    feat: remove markdown linting errors and add v1 tabline

--- lua/kickstart/plugins/bufferline.lua
+++
@@ -1,6 +0,0 @@
--- Neo-tree is a Neovim plugin to browse the file system
--- https://github.com/nvim-neo-tree/neo-tree.nvim
-
-return {
-  { 'akinsho/bufferline.nvim', version = '*', dependencies = 'nvim-tree/nvim-web-devicons' },
-}
