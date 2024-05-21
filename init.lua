267d12a5138dbcb6194c404febb2f1e6401f15bb
Jacob Elias <jacob@jacobs-apple-macbook-pro.phoenix-vibes.ts.net>
2024-05-21 10:08:26 -0500

    feat: coloscheme picking wiht preview is working

--- init.lua
+++ init.lua
@@ -339,12 +339,8 @@ require('lazy').setup({
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = true },
-      pickers = {
-        colorscheme = {
-          enable_preview = true,
-        },
-      },
     },
+
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
       -- it can fuzzy find! It's more than just a "file finder", it can search
@@ -370,6 +366,11 @@ require('lazy').setup({
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
+        pickers = {
+          colorscheme = {
+            enable_preview = true,
+          },
+        },
         --
         -- defaults = {
         --   mappings = {
