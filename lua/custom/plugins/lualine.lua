9f7df4ce39678e44b791d129225cdcb0cd75b77c
Jacob Elias <jacob@jacobs-apple-macbook-pro.phoenix-vibes.ts.net>
2024-05-26 16:12:54 -0500

    feat: stautsline looking 451

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -10,18 +10,20 @@ return {
       options = {
         theme = 'auto',
         component_separators = '',
+        globalstatus = true,
         section_separators = { left = '', right = '' },
       },
       sections = {
         lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-        lualine_b = { 'filename', 'branch' },
+        lualine_b = { {'branch', separator = { right = ''}}},
         lualine_c = {
-          '%=', --[[ add your center compoentnts here in place of this comment ]]
+          { 'filename', path = 1}, --[[ add your center compoentnts here in place of this comment ]]
         },
         lualine_x = {'diff', 'diagnostics'},
-        lualine_y = { 'filetype', 'progress' },
+        lualine_y = { 'filetype'},
         lualine_z = {
-          { 'location', separator = { right = '' }, left_padding = 2 },
+          { 'location', left_padding = 2 },
+          { 'progress', separator = { right = '' } },
         },
       },
       inactive_sections = {
